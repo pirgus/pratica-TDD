3c1415e1674fcaa41261d42d35332e4bae000139
pirgus <jaquecavaller@gmail.com>
2023-06-27 14:51:11 -0300

    Testes passando!

--- Money.java
+++ Money.java
@@ -1,58 +1,31 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+class Money  {
+    Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
+   }
 
-abstract class Money  {
    protected int amount;
    protected String currency;
    
    public boolean equals(Object object) {
       Money money = (Money) object;
-      return amount == money.amount && getClass().equals(money.getClass());
+      return amount == money.amount && currency().equals(money.currency());
    }
 
-    static Dollar dollar(int amount)  {
-      return new Dollar(amount, "USD");
+    Money times(int multiplier) {
+      return new Money(amount * multiplier, currency);
+   }
+
+    static Money dollar(int amount)  {
+      return new Money(amount, "USD");
    }
 
     static Money franc(int amount) {
-      return new Franc(amount, "CHF");
+      return new Money(amount, "CHF");
     }
 
     String currency(){
         return currency;
-    }
+    }  
 
-   Money(int amount, String currency) {
-      this.amount = amount;
-      this.currency = currency;
-   }
-
-   abstract Money times(int multiplier);  
-
-}
-
-public void testEquality() {
-   assertTrue(new Dollar(5).equals(new Dollar(5)));
-   assertFalse(new Dollar(5).equals(new Dollar(6)));
-   assertTrue(new Franc(5).equals(new Franc(5)));
-   assertFalse(new Franc(5).equals(new Franc(6)));
-   assertFalse(new Franc(5).equals(new Dollar(5)));
-}
-
-public void testMultiplication() {
-   Money five = Money.dollar(5);
-   assertEquals(Money.dollar(10), five.times(2));
-   assertEquals(Money.dollar(15), five.times(3));
-}
-
-public void testFrancMultiplication() {
-   Money five = Money.franc(5);
-   assertEquals(Money.franc(10), five.times(2));
-   assertEquals(Money.franc(15), five.times(3));
-}
-
-public void testCurrency() {
-   assertEquals("USD", Money.dollar(1).currency());
-   assertEquals("CHF", Money.franc(1).currency());
 }
